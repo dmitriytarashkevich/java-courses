7deae88b2dd4399cb23fb51d04e2b64d4c5121fd
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2016-03-20 18:07:50 +0300

    BaseFunctionsDouble

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,24 @@
 public class Calculate {
-    public static void main(String[] arg) {
-        System.out.println("Calculate...");
-        int a = Integer.valueOf(arg[0]);
-        int b = Integer.valueOf(arg[1]);
-        int sum=a+b;
-        System.out.println("Sum " + sum);
+    public static void main(String[] args) {
+        double a = Double.valueOf(args[0]);
+        double b = Double.valueOf(args[2]);
+        double result = 0;
+        if (args[1].equals("+")){
+            result=a+b;
+        }
+        if (args[1].equals("-")){
+            result = a-b;
+        }
+        if (args[1].equals("*")){
+            result = a*b;
+        }
+        if (args[1].equals("/")){
+            result = a/b;
+        }
+        if (args[1].equals("^")){
+            result = Math.pow(a, b);
+        }
+
+        System.out.println("Result: " + result);
     }
 }
