25948ae2fd867b9ccce5b6c14a8db13d5cdbefaa
dmitriytarashkevich <dmitriy.tarashkevich.95@mail.ru>
2016-03-20 16:47:31 +0300

    SumInt

--- Calculate.java
+++ Calculate.java
@@ -1,4 +1,9 @@
 public class Calculate {
-public static void main(String[] args) {
-System.out.println("Calculate"); }
+    public static void main(String[] arg) {
+        System.out.println("Calculate...");
+        int a = Integer.valueOf(arg[0]);
+        int b = Integer.valueOf(arg[1]);
+        int sum=a+b;
+        System.out.println("Sum " + sum);
+    }
 }
